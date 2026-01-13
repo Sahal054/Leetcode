8e7f4a842dee5a69a778982c50673c57d746e9ee
Sahal M <sahalmsachu@gmail.com>
2026-01-13 17:13:07 +0530

    added thought process to 26

--- 26_RemoveDuplicatesfromSortedArray.py
+++ 26_RemoveDuplicatesfromSortedArray.py
@@ -1,3 +1,25 @@
+"""
+Docstring for 26_RemoveDuplicatesfromSortedArray
+
+We use a 2 pointer approach for this.
+
+[0,0,1,1,1,2,2,3,3,4]
+   L
+   R
+
+[0,0,1,1,1,2,2,3,3,4] --> [0,1,_,1,1,2,2,3,3,4] 
+   L R                       L  R
+
+the trick is to find all the uniqe elements in the array. this is being done by the L pointer.
+
+we dont replace the same element in R to L instead if replace the UNIQUE element.
+
+we search L and R if L and R is the same R shifts next. IF is not the same then whats at R is shifted to L.
+
+end we return L
+
+"""
+
 class Solution(object):
     def removeDuplicates(self, nums):
         l = 1

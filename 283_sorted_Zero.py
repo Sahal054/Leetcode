f64f26dc9f85e0fb198c6a0dd5e823eafa31f6c0
Sahal M <sahalmsachu@gmail.com>
2026-01-16 17:33:20 +0530

    added thought process to 283

--- 283_sorted_Zero.py
+++ 283_sorted_Zero.py
@@ -1,9 +1,26 @@
+"""
+Docstring for 283_sorted_Zero
+ The logic is relativly simple,
+  we use 2 pointers. if a number is a number that is not 0 u swap it with L and then you incriment L
+
+        [0,1,0,3,12]
+         L  R
+        [1,0,0,3,12]
+           L R
+        [1,0,0,3,12]
+           L   R
+        [1,3,0,0,12]   
+             L    R
+        [1,3,12,0]
+              L,R     
+"""
+
 class Solution(object):
     def moveZeroes(self, nums):
         l =0
 
         for r in range(0,len(nums)):
-            if nums[r]:
+            if nums[r]: # or nums[r] !=0
                 nums[l], nums[r] = nums[r],nums[l]
                 l+=1
 

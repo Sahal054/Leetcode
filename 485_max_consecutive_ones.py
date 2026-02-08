bea375bd53bbb72651fe4463d9919cc8ad2aa8b7
Sahal M <sahalmsachu@gmail.com>
2026-02-09 05:12:14 +0530

    added thought process to 485

--- 485_max_consecutive_ones.py
+++ 485_max_consecutive_ones.py
@@ -1,3 +1,33 @@
+"""
+Docstring for 485_max_consecutive_ones
+
+
+
+relativly simple one 
+
+2 ways one is to use the count method this is self explnatory
+
+
+the other is to use the window method
+
+[ 1 , 1 , 0 , 1 , 1 , 1 ]
+  l   r
+[ 1 , 1 , 0 , 1 , 1 , 1 ]  
+  l       r
+
+  this is why we use r+1 other wise
+          l be here.  
+[ 1 , 1 , 0 , 1 , 1 , 1 ]  
+              l
+              r
+
+
+
+
+"""
+
+
+
 class Solution(object):
     def findMaxConsecutiveOnes(self, nums):
         count  = 0

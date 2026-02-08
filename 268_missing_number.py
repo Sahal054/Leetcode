0457fa806c8515eecdaa224727195b3819a70be4
Sahal M <sahalmsachu@gmail.com>
2026-02-09 04:43:11 +0530

    added a understanding for the question 268

--- 268_missing_number.py
+++ 268_missing_number.py
@@ -1,3 +1,43 @@
+"""
+Docstring for 268_missing_number
+
+so there are 2 ways to solve this one is use the xor operator 
+
+when you xor two numsbers if its same u get 0 if not you get 1 ie, 
+1 1 0 
+1 0 0                                                                   
+------
+0 1 0
+
+
+eg 5^5 = 0, 
+   4^3 = 1
+
+how so? take  5^5^3 = 3
+
+any number xor 0 is that number
+1 0 1  -5
+1 0 1  -5
+-----
+0 0 0
+0 1 1  -3
+-----
+0 1 1 -3
+
+
+hence we can xor [3,0,1] with [0,1,2,3]  or(len of n)
+
+
+
+
+Option 2 is 
+
+sum[n] - sum[len(n)]  = missing number 
+
+this can be found with eq s = n*(n+1)//2
+""" 
+
+
 class Solution(object):
     def missingNumber(self, nums):
         s = set(nums)
